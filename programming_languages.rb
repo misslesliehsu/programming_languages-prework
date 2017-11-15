d5d2edfae509a5c6fec94c09276e8e7d3f6071c7
Leslie Hsu <miss.leslie.hsu@gmail.com>
2017-11-15 02:47:05 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
+#ORIGINAL
+#style => language => type
+#DESIRED
+#language => type => style
+require 'pry'
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style_key, languages_hash| #oo => {ruby=>{type=>compiled}, javascript =>{type=>interpreted}}
+    languages_hash.each do |language_key, type_hash| #ruby => {type=>compiled}
+      if new_hash.include?(language_key)
+        new_hash[language_key][:style] << style_key
+      else
+        type_hash[:style] = [style_key]
+        new_hash[language_key] = type_hash
+      end
+    end
+  end
+   new_hash
 end
