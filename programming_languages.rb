b910ab70255e643e2a206e5d082c366000a0a38f
Leslie Hsu <miss.leslie.hsu@gmail.com>
2017-11-15 02:41:48 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,7 +5,7 @@
 require 'pry'
 def reformat_languages(languages)
   new_hash = {}
-  binding_pry
+  binding.pry
   languages.each do |style_key, languages_hash| #oo => {ruby=>{type=>compiled}, javascript =>{type=>interpreted}}
     languages_hash.each do |language_key, type_hash| #ruby => {type=>compiled}
       if new_hash.include?(language_key)
