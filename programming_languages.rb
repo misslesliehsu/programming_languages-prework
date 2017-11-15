6387b013061a73cfece718161634922f913e0441
Leslie Hsu <miss.leslie.hsu@gmail.com>
2017-11-15 01:52:41 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,11 @@
+#ORIGINAL
+#style => language => type
+#DESIRED
+#language => type => style
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, language|
+   new_hash[language] = style
+   binding.pry
 end
